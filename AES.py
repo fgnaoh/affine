dc977d75f4b991ed313aceccc5591393e9fa4b1a
fgnaoh <curry342004@gmail.com>
2024-10-23 13:44:31 +0700

    Update AES.py

--- AES.py
+++ AES.py
@@ -1,29 +1,18 @@
-<!-- product_form.html -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Product Form</title>
-</head>
-<body>
-    <h2>Nhập thông tin sản phẩm</h2>
-    <form action="save_product.php" method="post" enctype="multipart/form-data">
-        Mã hàng: <input type="text" name="ma_hang" required><br>
-        Tên hàng: <input type="text" name="ten_hang" required><br>
-        Nhóm hàng: 
-        <select name="nhom_hang">
-            <?php
-            $lines = file('category.txt');
-            foreach ($lines as $line) {
-                list($ma_nhom, $ten_nhom) = explode("\t", trim($line));
-                echo "<option value=\"$ma_nhom\">$ten_nhom</option>";
-            }
-            ?>
-        </select><br>
-        Số lượng: <input type="number" name="so_luong" required><br>
-        Đơn giá: <input type="number" name="don_gia" required><br>
-        Hình ảnh: <input type="file" name="hinh_anh" accept="image/*"><br>
-        <input type="submit" value="Lưu sản phẩm">
-    </form>
-</body>
-</html>
+<?php
+// save_product.php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $ma_hang = $_POST['ma_hang'];
+    $ten_hang = $_POST['ten_hang'];
+    $nhom_hang = $_POST['nhom_hang'];
+    $so_luong = $_POST['so_luong'];
+    $don_gia = $_POST['don_gia'];
+    $hinh_anh = $_FILES['hinh_anh']['name'];
+
+    move_uploaded_file($_FILES['hinh_anh']['tmp_name'], "uploads/$hinh_anh");
+
+    $data = "$ma_hang\t$ten_hang\t$nhom_hang\t$so_luong\t$don_gia\t$hinh_anh\n";
+    file_put_contents('hang.txt', $data, FILE_APPEND);
+
+    echo "Sản phẩm đã được lưu.";
+}
+?>
